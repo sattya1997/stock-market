c542bcbb0a492ed28294d6190773814316d2a307
Mr Devil <118550212+sattya1997@users.noreply.github.com>
2024-05-23 20:57:11 +0530

    Update extraTab.js

--- code/extraTab.js
+++ code/extraTab.js
@@ -15,7 +15,7 @@ foreignCheckbox.addEventListener('change', () => {
 });
 
 calendarCheckbox.addEventListener('change', () => {
-  if (checkbox.checked) {
+  if (calendarCheckbox.checked) {
     childElement.innerHTML = childElement.innerHTML = `<iframe src="https://sslecal2.investing.com?columns=exc_flags,exc_currency,exc_importance,exc_actual,exc_forecast,exc_previous&category=_employment,_economicActivity,_inflation,_credit,_centralBanks,_confidenceIndex,_balance,_Bonds&importance=1,2,3&features=datepicker,timezone,timeselector,filters&countries=14&calType=week&timeZone=23&lang=56" width="650" height="467" frameborder="0" allowtransparency="true" marginwidth="0" marginheight="0"></iframe><div class="poweredBy" style="font-family: Arial, Helvetica, sans-serif;"><span style="font-size: 11px;color: #333333;text-decoration: none;">Real Time Economic Calendar provided by <a href="https://in.Investing.com/" rel="nofollow" target="_blank" style="font-size: 11px;color: #06529D; font-weight: bold;" class="underline_link">Investing.com India</a>.</span></div>`
     parentDiv.appendChild(childElement);
   } else {
